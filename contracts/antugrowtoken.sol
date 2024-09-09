5d574c94c949eb96a86be76b764c770c93a59f2f
scripttie <otrasmali@gmail.com>
2024-09-09 23:47:36 +0300

    create antugrow token

---
+++ contracts/antugrowtoken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AntugrowToken is ERC20 {
+    constructor() ERC20("AntugrowToken", "ANTUG") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
