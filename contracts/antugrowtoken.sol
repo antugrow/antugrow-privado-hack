29dbcf309366d80825f6dbf885a4ac3a265fd692
scripttie <otrasmali@gmail.com>
2024-09-09 23:51:11 +0300

    update token ticker

--- contracts/antugrowtoken.sol
+++ contracts/antugrowtoken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AntugrowToken is ERC20 {
-    constructor() ERC20("AntugrowToken", "ANTUG") {
+    constructor() ERC20("AntugrowToken", "ANTG") {
         _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
