5ec83561a69b55e14cbd2d5197bb553dd63c0e6d
VinGitonga <gitongavincent64@gmail.com>
2024-09-11 03:04:59 +0300

    Updated investor home text

--- investor-ui/src/pages/Home.tsx
+++ investor-ui/src/pages/Home.tsx
@@ -7,11 +7,11 @@ const Home = () => {
 			<section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
 				<div className="text-center space-y-4">
 					<h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
-						Optimize your website for
-						<span className="text-indigo-600"> Search engine</span>
+						Empower Farmers
+						<span className="text-indigo-600"> in Africa</span>
 					</h1>
 					<p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
-						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum
+						Join Antugrow in transforming the lives of farmers across Africa. Discover how your investment can drive change, boost livelihoods, and create a brighter future for rural communities.
 					</p>
 				</div>
 				<div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
